07e42742671bb45f57a7c36f17bf073fdbe37680
nilesh <nileshdusane10@gmail.com>
2023-07-06 15:21:17 +0530

    msg

---
+++ src/main/java/com/example/spring_hello_world/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.spring_hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello World!!";
+    }
+}
